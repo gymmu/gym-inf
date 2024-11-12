7a2c4fcab61364162be2c7b020cca2a5e22035cb
Cedric Geissmann <cedric.geissmann@gmail.com>
2024-11-12 06:11:55 +0000

    Copy over static files.

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,27 @@
-import { defineConfig } from 'vite'
-import mdx from '@mdx-js/rollup'
-import react from '@vitejs/plugin-react'
+import { defineConfig, normalizePath } from "vite"
+import mdx from "@mdx-js/rollup"
+import react from "@vitejs/plugin-react"
+import { viteStaticCopy } from "vite-plugin-static-copy"
+import path from "node:path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    {enforce: 'pre', ...mdx(/* jsxImportSource: …, otherOptions… */)},
-    react()
+    { enforce: "pre", ...mdx(/* jsxImportSource: …, otherOptions… */) },
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: normalizePath(
+            path.resolve(__dirname, "src/sites/md") + "/*.mdx",
+          ),
+          dest: "./assets",
+        },
+      ],
+    }),
   ],
   build: {
-    outDir: 'docs',
+    outDir: "docs",
   },
   base: "/gym-inf/",
-  // assetsInclude: ["**/*.html"]
 })
