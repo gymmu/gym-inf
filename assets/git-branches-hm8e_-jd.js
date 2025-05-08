150613f67cd59f8bb86ba82e35967c9bf756abe7
Cedric Geissmann <cedric.geissmann@gmail.com>
2025-05-08 07:20:52 +0000

    Updates

--- assets/git-branches-hm8e_-jd.js
+++
@@ -1,9 +0,0 @@
-import{j as e}from"./index-cIQnBFSb.js";import{C as r}from"./Chapter-De4Fl3Be.js";import{E as t}from"./Example-CKB1bqIz.js";import{V as c}from"./Video-BVvtb2Gt.js";function i(s){const n={code:"code",h2:"h2",p:"p",strong:"strong",...s.components};return e.jsxs(r,{children:[e.jsxs(n.h2,{children:["Versionsverzweigungen (",e.jsx(n.code,{children:"branches"}),")"]}),e.jsxs(n.p,{children:[e.jsx(n.strong,{children:"Git"})," operiert auf sogenannten ",e.jsx(n.code,{children:"branch"}),"es. Ein ",e.jsx(n.code,{children:"branch"}),` ist ein Zweig im
-Versionsverlauf, was nichts anderes bedeutet als dass alle `,e.jsx(n.code,{children:"commit"}),`s auf diesem
-Zweig angeordnet sind.`]}),e.jsxs(n.p,{children:["Man kann in ",e.jsx(n.strong,{children:"Git"})," mehr wie nur einen ",e.jsx(n.code,{children:"branch"}),` haben, dadurch können mehrere
-Personen an einem Projekt arbeiten, oder man kann verschiedene Entwicklungen zur
-gleichen Zeit erproben. Dies kann praktisch sein, wenn man verschiedene Designs
-testen möchte.`]}),e.jsxs(t,{title:"Beispiel: 2 unterschiedliche Designs entwickeln und vergleichen",children:[e.jsxs(n.p,{children:["Ein einfacher Einstieg in ",e.jsx(n.code,{children:"branch"}),`es, ist es 2 unterschiedliche Versionen von
-einem Projekt zu erstellen, und diese zu vergleichen. Am einfachsten geht das
-mit dem Design.`]}),e.jsxs(n.p,{children:["Das folgende Video zeigt wie man 2 ",e.jsx(n.code,{children:"branch"}),`es erstellt, und diese dann
-vergleicht.`]}),e.jsx(c,{url:"ZGymWLlR6VE"})]})]})}function l(s={}){const{wrapper:n}=s.components||{};return n?e.jsx(n,{...s,children:e.jsx(i,{...s})}):i(s)}export{l as default};
