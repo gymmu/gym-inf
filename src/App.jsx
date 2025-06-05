e234bf804e103eeae6ec87ecb20bb6aecf1beafb
Cedric Geissmann <cedric.geissmann@gmail.com>
2025-06-05 17:30:32 +0200

    Einfachere routes, nur an einer Stelle.

--- src/App.jsx
+++ src/App.jsx
@@ -2,37 +2,72 @@ import {
   Outlet,
   createBrowserRouter,
   RouterProvider,
+  Navigate,
 } from "react-router-dom"
+import { useState, useEffect } from "react"
 import { NavProvider } from "./context/NavContext"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import Navbar from "./components/Navbar"
 
 // Import routes from external file
-import { routeRegistry } from './routes';
-
+import { routeRegistry } from "./routes"
 
 export default function App() {
-    // Create the router using the registry
-    // Fügen Sie die Routen hier ein
-    const router = createBrowserRouter(
-        [
-            {
-                path: "/",
-                element: <Layout />,  
-                children: routeRegistry,
-            },
-        ],
+  const [router, setRouter] = useState(null)
+  const [routeCount, setRouteCount] = useState(routeRegistry.length)
+
+  // Function to create/recreate the router
+  const createRouter = () => {
+    return createBrowserRouter(
+      [
         {
-            basename: "/gym-inf",
+          path: "/",
+          element: <Layout />,
+          children: [
+            ...routeRegistry, // Create a new array to ensure fresh reference
+            {
+              path: "*", // Catch-all route for invalid paths
+              element: <Navigate to="/" replace />,
+            },
+          ],
         },
-    );
-    return <RouterProvider router={router} />
+      ],
+      {
+        basename: "/gym-inf",
+      },
+    )
+  }
+
+  // Initialize router
+  useEffect(() => {
+    setRouter(createRouter())
+  }, [])
+
+  // Check for route changes and recreate router if needed
+  useEffect(() => {
+    const checkRoutes = () => {
+      if (routeRegistry.length !== routeCount) {
+        setRouteCount(routeRegistry.length)
+        setRouter(createRouter())
+      }
+    }
+
+    // Check every 100ms for new routes
+    const interval = setInterval(checkRoutes, 100)
+
+    return () => clearInterval(interval)
+  }, [routeCount])
+
+  if (!router) {
+    return <div>Loading...</div>
+  }
+
+  return <RouterProvider router={router} />
 }
 
 // Layout component can be extracted
 function Layout() {
-
   return (
     <>
       <NavProvider>
@@ -46,8 +81,3 @@ function Layout() {
     </>
   )
 }
-
-import NavLink from "./components/NavLink";
-
-// NavLink component can be extracted
-
--- src/components/ChapterIndex.jsx
+++ src/components/ChapterIndex.jsx
@@ -1,5 +1,5 @@
-import NavLink from "./NavLink";
-import ICode from "./ICode";
+import NavLink from "./NavLink"
+import ICode from "./ICode"
 
 function ChapterIndex() {
   return (
@@ -14,46 +14,76 @@ function ChapterIndex() {
         <li>
           Einführung
           <ol>
-            <NavLink to="install">Installation</NavLink>
+            <NavLink to="install" site="../sites/md/installation.mdx">
+              Installation
+            </NavLink>
           </ol>
         </li>
         <li>
           HTML
           <ol>
-            <NavLink to="html" site="../sites/md/html.mdx">Webseiten</NavLink>
-            <NavLink to="html-elements">Webseiten strukturieren</NavLink>
-            <NavLink to="html-attributes">HTML Attribute</NavLink>
+            <NavLink to="html" site="../sites/md/html.mdx">
+              Webseiten
+            </NavLink>
+            <NavLink to="html-elements" site="../sites/md/html-elements.mdx">
+              Webseiten strukturieren
+            </NavLink>
+            <NavLink
+              to="html-attributes"
+              site="../sites/md/html-attributes.mdx">
+              HTML Attribute
+            </NavLink>
           </ol>
         </li>
         <li>
           SVG
           <ol>
-            <NavLink to="svg">Vektorgrafiken mit SVG</NavLink>
-            <NavLink to="svg-elements">Weitere SVG-Elemente</NavLink>
-            <NavLink to="svg-clipping">Elemente verbergen</NavLink>
-            <NavLink to="svg-animation">Elemente animieren</NavLink>
+            <NavLink to="svg" site="./sites/md/svg.mdx">
+              Vektorgrafiken mit SVG
+            </NavLink>
+            <NavLink to="svg-elements" site="./sites/md/svg-elements.mdx">
+              Weitere SVG-Elemente
+            </NavLink>
+            <NavLink to="svg-clipping" site="./sites/md/svg-clipping.mdx">
+              Elemente verbergen
+            </NavLink>
+            <NavLink to="svg-animation" site="./sites/md/svg-animation.mdx">
+              Elemente animieren
+            </NavLink>
           </ol>
         </li>
         <li>
           CSS
           <ol>
-            <NavLink to="css">Elemente gestalten</NavLink>
-            <NavLink to="css-selectors">CSS Selektoren</NavLink>
-            <NavLink to="css-box-model">CSS Box Modell</NavLink>
-            <NavLink to="css-animations">CSS Animationen</NavLink>
+            <NavLink to="css" site="./sites/md/css.mdx">
+              Elemente gestalten
+            </NavLink>
+            <NavLink to="css-selectors" site="./sites/md/css-selectors.mdx">
+              CSS Selektoren
+            </NavLink>
+            <NavLink to="css-box-model" site="./sites/md/css-box-model.mdx">
+              CSS Box Modell
+            </NavLink>
+            <NavLink to="css-animations" site="./sites/md/css-animationen.mdx">
+              CSS Animationen
+            </NavLink>
           </ol>
         </li>
         <li>
           Git
           <ol>
-            <NavLink to="git">Versionskontrolle</NavLink>
-            <NavLink to="git-branches">
+            <NavLink to="git" site="./sites/md/git.mdx">
+              Versionskontrolle
+            </NavLink>
+            <NavLink to="git-branches" site="./sites/md/git-branches.mdx">
               Versionszweige (<ICode>branch</ICode>es)
             </NavLink>
-            <NavLink to="git-merges">
+            <NavLink to="git-merges" site="./sites/md/git-merges.mdx">
               Versionen zusammenführen (<ICode>merge</ICode>)
             </NavLink>
-            <NavLink to="git-merge-conflicts">
+            <NavLink
+              to="git-merge-conflicts"
+              site="./sites/md/git-merge-conflicts.mdx">
               <ICode>merge</ICode>-Konflikte
             </NavLink>
           </ol>
@@ -61,20 +91,38 @@ function ChapterIndex() {
         <li>
           Tipps und Tricks für HTML und CSS
           <ol>
-            <NavLink to="tipp-images">Bilder zentrieren</NavLink>
-            <NavLink to="tipp-flexbox">Layouts</NavLink>
-            <NavLink to="img-background">Bild als Hintergrund</NavLink>
-            <NavLink to="img-next-to-text">Bild neben Text</NavLink>
+            <NavLink to="tipp-images" site="./sites/md/css-img.mdx">
+              Bilder zentrieren
+            </NavLink>
+            <NavLink to="tipp-flexbox" site="./sites/md/css-layout.mdx">
+              Layouts
+            </NavLink>
+            <NavLink
+              to="img-background"
+              site="./sites/md/css-img-background.mdx">
+              Bild als Hintergrund
+            </NavLink>
+            <NavLink
+              to="img-next-to-text"
+              site="./sites/md/css-img-next-to-text.mdx">
+              Bild neben Text
+            </NavLink>
           </ol>
         </li>
         <li>
           Javascript
           <ol>
-            <NavLink to="javascript">Einführung in Javascript</NavLink>
-            <NavLink to="javascript-change-page">
+            <NavLink to="javascript" site="./sites/md/javascript.mdx">
+              Einführung in Javascript
+            </NavLink>
+            <NavLink
+              to="javascript-change-page"
+              site="./sites/md/javascript-change-page.mdx">
               Seiteninhalte mit Javascript anpassen
             </NavLink>
-            <NavLink to="javascript-text-to-numbers">
+            <NavLink
+              to="javascript-text-to-numbers"
+              site="./sites/md/javascript-text-to-numbers.mdx">
               Datentypen in Javascript
             </NavLink>
           </ol>
@@ -82,57 +130,117 @@ function ChapterIndex() {
         <li>
           Javascript: Logik
           <ol>
-            <NavLink to="javascript-variables">Variablen</NavLink>
-            <NavLink to="javascript-blocks">Code-Blöcke / Scoping</NavLink>
-            <NavLink to="javascript-declarative">
+            <NavLink
+              to="javascript-variables"
+              site="./sites/md/javascript-variables.mdx">
+              Variablen
+            </NavLink>
+            <NavLink
+              to="javascript-blocks"
+              site="./sites/md/javascript-blocks.mdx">
+              Code-Blöcke / Scoping
+            </NavLink>
+            <NavLink
+              to="javascript-declarative"
+              site="./sites/md/javascript-declarative.mdx">
               Deklarativer Code und praktische Funktionen
             </NavLink>
-            <NavLink to="javascript-if">
+            <NavLink to="javascript-if" site="./sites/md/javascript-if.mdx">
               Bedingungen (<pre>if</pre>)
             </NavLink>
-            <NavLink to="javascript-for">
+            <NavLink to="javascript-for" site="./sites/md/javascript-for.mdx">
               Javascript Schleifen (<pre>for</pre>)
             </NavLink>
-            <NavLink to="javascript-return">
+            <NavLink
+              to="javascript-return"
+              site="./sites/md/javascript-return.mdx">
               Rückgabewerte in Javascript (<pre>return</pre>)
             </NavLink>
-            <NavLink to="javascript-lists">Listen in Javascript</NavLink>
-            <NavLink to="javascript-ascii">ASCII-Codes in Javascript</NavLink>
-            <NavLink to="javascript-switches">Schalter in Javascript</NavLink>
+            <NavLink
+              to="javascript-lists"
+              site="./sites/md/javascript-lists.mdx">
+              Listen in Javascript
+            </NavLink>
+            <NavLink
+              to="javascript-ascii"
+              site="./sites/md/javascript-ascii.mdx">
+              ASCII-Codes in Javascript
+            </NavLink>
+            <NavLink
+              to="javascript-switches"
+              site="./sites/md/javascript-switches.mdx">
+              Schalter in Javascript
+            </NavLink>
           </ol>
         </li>
         <li>
           Künstliche Intelligenz
           <ol>
-            <NavLink to="ai-intro">Arbeiten mit küstlicher Intelligenz</NavLink>
-            <NavLink to="ai-pong">Pong mit küstlicher Intelligenz</NavLink>
+            <NavLink to="ai-intro" site="./sites/md/ai-intro.mdx">
+              Arbeiten mit küstlicher Intelligenz
+            </NavLink>
+            <NavLink to="ai-pong" site="./sites/md/ai-pong.mdx">
+              Pong mit küstlicher Intelligenz
+            </NavLink>
           </ol>
         </li>
         <li>
           Daten und Objekte
           <ol>
-            <NavLink to="data">Daten</NavLink>
-            <NavLink to="data-objects">Objekte</NavLink>
-            <NavLink to="data-complex">Komplexe Daten</NavLink>
-            <NavLink to="data-classes">Klassen</NavLink>
-            <NavLink to="data-objects-as-data">Objekte as Datenquelle</NavLink>
-            <NavLink to="data-binary">Binärsystem</NavLink>
-            <NavLink to="data-hex">Hexadezimalsystem</NavLink>
+            <NavLink to="data" site="./sites/md/data.mdx">
+              Daten
+            </NavLink>
+            <NavLink to="data-objects" site="./sites/md/data-objects.mdx">
+              Objekte
+            </NavLink>
+            <NavLink to="data-complex" site="./sites/md/data-complex.mdx">
+              Komplexe Daten
+            </NavLink>
+            <NavLink to="data-classes" site="./sites/md/data-classes.mdx">
+              Klassen
+            </NavLink>
+            <NavLink
+              to="data-objects-as-data"
+              site="./sites/md/data-objects-as-data.mdx">
+              Objekte as Datenquelle
+            </NavLink>
+            <NavLink to="data-binary" site="./sites/md/data-binary.mdx">
+              Binärsystem
+            </NavLink>
+            <NavLink to="data-hex" site="./sites/md/data-hex.mdx">
+              Hexadezimalsystem
+            </NavLink>
           </ol>
         </li>
         <li>
           Computerspiele
           <ol>
-            <NavLink to="game">Eigenes Computerspiel</NavLink>
-            <NavLink to="game-animations">Spieler und Animationen</NavLink>
-            <NavLink to="game-interactions">Interaktionen im Spiel</NavLink>
+            <NavLink to="game" site="./sites/md/game.mdx">
+              Eigenes Computerspiel
+            </NavLink>
+            <NavLink
+              to="game-animations"
+              site="./sites/md/game-player-and-animations.mdx">
+              Spieler und Animationen
+            </NavLink>
+            <NavLink
+              to="game-interactions"
+              site="./sites/md/game-interactions.mdx">
+              Interaktionen im Spiel
+            </NavLink>
           </ol>
         </li>
         <li>
           Sicherheit
           <ol>
-            <NavLink to="security-passwords">Passwörter und Sicherheit</NavLink>
-            <NavLink to="security-encryption">
+            <NavLink
+              to="security-passwords"
+              site="./sites/md/security-passwords.mdx">
+              Passwörter und Sicherheit
+            </NavLink>
+            <NavLink
+              to="security-encryption"
+              site="./sites/md/security-encryption.mdx">
               Verschiedene Verschlüsselungsverfahren
             </NavLink>
           </ol>
@@ -140,13 +248,14 @@ function ChapterIndex() {
         <li>
           Netzwerke
           <ol>
-            <NavLink to="network-intro">Computernetzwerke</NavLink>
+            <NavLink to="network-intro" site="./sites/md/network-intro.mdx">
+              Computernetzwerke
+            </NavLink>
           </ol>
         </li>
       </ol>
     </>
-  );
+  )
 }
 
-export default ChapterIndex;
-
+export default ChapterIndex
