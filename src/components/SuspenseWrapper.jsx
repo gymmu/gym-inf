2539fdb84776e711794cd232df5c9ec292bac004
Cedric Geissmann <cedric.geissmann@gmail.com>
2025-08-10 12:39:04 +0200

    Try to fix SuspenseWrapper.

--- src/components/SuspenseWrapper.jsx
+++ src/components/SuspenseWrapper.jsx
@@ -1,7 +1,8 @@
-import React, { lazy, Suspense } from 'react';
+import React, { lazy, Suspense } from "react"
+import { ErrorBoundary } from "react-error-boundary"
 
 /* Suspend the loading of a component, used for lazy loading pages.
- * 
+ *
  * Mit dieser Komponente können Unterwebseite verzögert geladen werden. So wird
  * das ganze ein wenig dynamischer und nicht alle Inhalte müssen bereits beim
  * Start der Webseite verfügbar sein. Das ist wichtig da einige Unterwebseiten
@@ -10,15 +11,21 @@ import React, { lazy, Suspense } from 'react';
  */
 function SuspenseWrapper({ filename, fms = false }) {
   const file = filename.split("/").slice(-1)[0]
-  const basename = file.split(".")[0];
-  const finalFilename = fms ?  `../sites/md/fms/${basename}.mdx` : `../sites/md/${basename}.mdx`
-  const LazyComponent = lazy(() => import(`${finalFilename}`));
+  const basename = file.split(".")[0]
+  const finalFilename = fms
+    ? `/gym-inf/docs/assets/sites/md/fms/${basename}.mdx`
+    : `/gym-inf/docs/assets/sites/md/${basename}.mdx`
+  console.log(`Loading component from: ${finalFilename}`)
+  const LazyComponent = lazy(() =>
+    import(`${finalFilename}`).catch((err) => console.error(err)),
+  )
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <LazyComponent />
-    </Suspense>
-  );
+    <ErrorBoundary fallback={<div>Something went wrong</div>}>
+      <Suspense fallback={<div>Loading...</div>}>
+        <LazyComponent />
+      </Suspense>
+    </ErrorBoundary>
+  )
 }
 
-export default SuspenseWrapper;
-
+export default SuspenseWrapper
