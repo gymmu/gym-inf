e1d15ddbd17babc2e23a00cee9cebf88f86e62ec
Cedric Geissmann <cedric.geissmann@gmail.com>
2025-11-09 20:23:03 +0100

    Finished flexboxmodell.

--- src/components/FlexboxModel.jsx
+++ src/components/FlexboxModel.jsx
@@ -5,103 +5,162 @@ import { dark } from "react-syntax-highlighter/dist/esm/styles/prism"
 
 import Slider from "./Slider"
 import Select from "@components/Select.jsx"
+import Editor from "@components/Editor.jsx"
 import style from "@components/BoxModel.module.css"
 
-export default function BoxModel({ children }) {
-  const [bgColor, setBgColor] = useState("red")
+export default function FlexboxModel() {
+  const [srcDoc, setSrcDoc] = useState("")
   const [codeString, setCodeString] = useState("")
-  const [margin, setMargin] = useState(10)
-  const [border, setBorder] = useState(5)
-  const [padding, setPadding] = useState(20)
-  const [width, setWidth] = useState(200)
-  const [height, setHeight] = useState(150)
+  const [display, setDisplay] = useState("flex")
+  const [flexDirection, setFlexDirection] = useState("row")
+  const [gap, setGap] = useState(16)
+  const [alignItems, setAlignItems] = useState("normal") 
+  const [justifyContent, setJustifyContent] = useState("flex-start") 
+  const [wrap, setWrap] = useState("nowrap") 
+  const [grow, setGrow] = useState(0) 
+  const [shrink, setShrink] = useState(1) 
+  
+  const htmlString = `<div class="flex-wrapper">
+    <div>box 1</div>
+    <div>box 2</div>
+    <div>box 3</div>
+    <div>box 4</div>
+    <div>box 5</div>
+    <div>box 6</div>
+</div>`
 
   useEffect(() => {
-    const newCodeString = `.box {
-    margin: ${margin}px;
-    border: ${border}px solid black;
-    padding: ${padding}px;
-    width: ${width}px;
-    height: ${height}px;
-    background-color: ${bgColor};
+    const newCSS = `.flex-wrapper {
+    display: ${display};
+    flex-direction: ${flexDirection};
+    gap: ${gap}px;
+    align-items: ${alignItems};
+    justify-content: ${justifyContent};
+    flex-wrap: ${wrap};
+}
+
+.flex-wrapper > div {
+    flex-grow: ${grow};
+    flex-shrink: ${shrink};
+    flex-basis: auto;
 }`
+    handleCodeUpdate(newCSS)
 
-    setCodeString(newCodeString)
-    // Update previous values
-  }, [bgColor, margin, border, padding, width, height])
+  }, [display, flexDirection, gap, alignItems, justifyContent, wrap, grow, shrink])
 
-  return (
-    <div>
-      <div className={style.gridContainer}>
-                <div className={style.leftContainer}>
-        <div className={style.controlls}>
+    function handleCodeUpdate(code) {
+    setCodeString(code)
+
+    setSrcDoc(`<html>
+    <body>${htmlString}</body>
+    <style>${code}</style>
+    <style>
+body {
+    margin: 0;
+    padding: 0;
+}
+.flex-wrapper > div {
+    background-color: blue;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 3rem;
+    color: white;
+    font-size: 1.6rem;
+}</style>
+</html>`)
+
+    }
+
+    return (
+        <div className={style.gridContainer}>
+            <div style={{gridArea: "controlls"}} className={style.controlls}>
+                <h3>Kontrollelemente</h3>
                 <div className={style.formControll}>
-                    <label>Inhaltsfarbe: </label>
-          <Select
-            value={bgColor}
-            onChange={(e) => setBgColor(e.target.value)}
-            options={["black", "red", "green", "blue", "white"]}
-          />
-          </div>
+                    <label>Display: </label>
+                    <Select
+                        value={display}
+                        onChange={(e) => setDisplay(e.target.value)}
+                        options={["block", "flex"]}
+                    />
+                </div>
                 <div className={style.formControll}>
-          <Slider
-            sliderText="Margin:"
-            value={margin}
-            setValue={setMargin}
-            minVal={0}
-            maxVal={40}
-          />
-          </div>
+                    <label>Flex Richtung: </label>
+                    <Select
+                        value={flexDirection}
+                        onChange={(e) => setFlexDirection(e.target.value)}
+                        options={["row", "column", "row-reverse", "column-reverse"]}
+                    />
+                </div>
                 <div className={style.formControll}>
-          <Slider
-            sliderText="Border:"
-            value={border}
-            setValue={setBorder}
-            minVal={0}
-            maxVal={20}
-          />
-          </div>
+                    <Slider
+                        sliderText="Abstand:"
+                        value={gap}
+                        setValue={setGap}
+                        minVal={0}
+                        maxVal={128} />
+                </div>
                 <div className={style.formControll}>
-          <Slider
-            sliderText="Padding:"
-            value={padding}
-            setValue={setPadding}
-            minVal={0}
-            maxVal={50}
-          />
-          </div>
+                    <label>Ausrichtung Hauptachse: </label>
+                    <Select
+                        value={justifyContent}
+                        onChange={(e) => setJustifyContent(e.target.value)}
+                        options={["flex-start", "center", "flex-end", "space-around", "space-between", "space-evenly"]}
+                    />
+                </div>
                 <div className={style.formControll}>
-          <Slider
-            sliderText="Width:"
-            value={width}
-            setValue={setWidth}
-            minVal={150}
-            maxVal={200}
-          />
-          </div>
+                    <label>Ausrichtung Cross-Achse: </label>
+                    <Select
+                        value={alignItems}
+                        onChange={(e) => setAlignItems(e.target.value)}
+                        options={["normal", "flex-start", "center", "flex-end", "strech", "baseline"]}
+                    />
+                </div>
                 <div className={style.formControll}>
-          <Slider
-            sliderText="Height:"
-            value={height}
-            setValue={setHeight}
-            minVal={150}
-            maxVal={300}
-          />
-          </div>
-          <SyntaxHighlighter language="css" style={dark}>
-            {codeString}
-          </SyntaxHighlighter>
+                    <label>Umbrechen: </label>
+                    <Select
+                        value={wrap}
+                        onChange={(e) => setWrap(e.target.value)}
+                        options={["nowrap", "wrap"]}
+                    />
+                </div>
+                <div className={style.formControll}>
+                    <Slider
+                        sliderText="Wachsen:"
+                        value={grow}
+                        setValue={setGrow}
+                        minVal={0}
+                        maxVal={5} />
+                </div>
+                <div className={style.formControll}>
+                    <Slider
+                        sliderText="Schrumpfen:"
+                        value={shrink}
+                        setValue={setShrink}
+                        minVal={0}
+                        maxVal={5} />
+                </div>
+            </div>
+            <div style={{gridArea: "css"}}>
+                <h3>CSS Code</h3>
+                <Editor title="CSS" language="css" value={codeString} handleChange={handleCodeUpdate} />
+            </div>
+            <div style={{gridArea: "html"}}>
+                <h3>HTML Code</h3>
+                <SyntaxHighlighter language="html" style={dark}>
+                    {htmlString}
+                </SyntaxHighlighter>
+            </div>
+            <div style={{gridArea: "iframe"}}>
+                <h3>Resultat</h3>
+                <iframe 
+                    srcDoc={srcDoc}
+                    title="output"
+                    frameBorder="0"
+                    height="100%"
+                    width="100%"
+                />
+            </div>
         </div>
-        </div>
-        <div className={style.flexWrapper}>
-        <div className={style.flexBox}>box 1</div>
-        <div className={style.flexBox}>box 2</div>
-        <div className={style.flexBox}>box 3</div>
-        <div className={style.flexBox}>box 4</div>
-        <div className={style.flexBox}>box 5</div>
-        <div className={style.flexBox}>box 6</div>
-        </div>
-      </div>
-    </div>
-  )
+    )
 }
