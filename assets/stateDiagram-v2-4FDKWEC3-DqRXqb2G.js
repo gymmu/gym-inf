ec9f054e8c6f3fe67d17927fce82ee7a4e869f4a
Cedric Geissmann <cedric.geissmann@gmail.com>
2025-11-14 07:23:00 +0100

    Updates

---
+++ assets/stateDiagram-v2-4FDKWEC3-DqRXqb2G.js
@@ -0,0 +1,1 @@
+import{s as t,b as r,a,S as s}from"./chunk-DI55MBZ5-Jp93Tqth.js";import{_ as i}from"./index-CS7QzTsb.js";import"./chunk-55IACEB6-tB5b5SEt.js";import"./chunk-QN33PNHL-Dhyz8DQo.js";var l={parser:a,get db(){return new s(2)},renderer:r,styles:t,init:i(e=>{e.state||(e.state={}),e.state.arrowMarkerAbsolute=e.arrowMarkerAbsolute},"init")};export{l as diagram};
