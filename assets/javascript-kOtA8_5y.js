490144f0c7f51812a8a7ac1ecf6b1fb5f099942c
Cedric Geissmann <cedric.geissmann@gmail.com>
2026-03-11 09:04:11 +0100

    Updates

--- assets/javascript-kOtA8_5y.js
+++
@@ -1,5 +0,0 @@
-import{j as e}from"./react-vendor-DPtW2uLn.js";import{V as r}from"./gym-pages-B1WatTOA.js";import"./vendor-BscfZStV.js";import"./monaco-DSiUpym4.js";function i(n){const t={h2:"h2",p:"p",...n.components};return e.jsxs(e.Fragment,{children:[e.jsx(t.h2,{children:"Dynamische Webseiten mit Javascript"}),`
-`,e.jsxs(t.p,{children:[`Die Programmiersprache Javascript wird verwendet um Webseiten dynamisch und
-interaktiv zu machen. Sie finden auf diesem `,e.jsx("a",{href:"https://codepen.io/Cedric-Geissmann/pen/EaKoMLX",target:"_blank",children:"CodePen"}),` eine Vorlage, die im unteren Video vorgestellt
-wird.`]}),`
-`,e.jsx(r,{url:"DURh9eYZAlk"})]})}function m(n={}){const{wrapper:t}=n.components||{};return t?e.jsx(t,{...n,children:e.jsx(i,{...n})}):i(n)}export{m as default};
